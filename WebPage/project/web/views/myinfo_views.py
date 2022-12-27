549f2675932f0c9b6f51b7b9b53cf96ead75dadf
BackFlash <53032619+0BackFlash0@users.noreply.github.com>
2022-12-27 14:18:03 +0900

    Merge branch 'kbj' into hs12-27

--- WebPage/project/web/views/myinfo_views.py
+++ WebPage/project/web/views/myinfo_views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, g, request, flash
 from ..db import WebProject
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash,generate_password_hash
 from ..form import Trypwd, Enroll
 
 bp = Blueprint('myinfo', __name__, url_prefix='/myinfo')
@@ -33,9 +33,19 @@ def password():
     
 @bp.route('/infomodify', methods=['GET', 'POST'])
 def infomodify():
+    if(request.method == 'POST'):
+        params = request.get_json()
+        if len(params['value']) == 0:
+            flash('다시 입력해 주세요')
+            return redirect(url_for("myinfo.infomodify"))
+        else:
+            if params['column'] == 'name':
+                wp.send_query("UPDATE user SET {} = '{}' WHERE id='{}'".format(params['column'], params['value'], g.user['user_id']), commit=True)
+            else:
+                wp.send_query("UPDATE user SET {} = '{}' WHERE id='{}'".format(params['column'], generate_password_hash(params['value']), g.user['user_id']), commit=True)
+            return redirect(url_for("myinfo.infomodify"))
     user_info = {}
     user = wp.send_query("select * from user where id='{}'".format(g.user['user_id']))
-
     user_info['user_name'] = user[0]['name']
     user_info['user_id'] = user[0]['id']
     user_info['user_pwd']= user[0]['pwd']
@@ -50,21 +60,20 @@ def notice():
     notice_dict = {}
     notice = wp.send_query("SELECT * FROM notice_board ORDER BY date DESC")
     notice_dict['item'] = notice
-    # user_id 가 아닌 admin을 확인 할 수 있는 무언가로 변경해야 한다다다다
-    notice_dict['admin'] = g.user['user_id']
+    user = wp.send_query("select * from user where id = '{}'".format(g.user['user_id']))
+    notice_dict['admin'] = user[0]['admin']
     return render_template('myinfo/myinfo_notice.html',notice = notice_dict)
 
 
 @bp.route('/content/<int:content_id>', methods=['GET'])
 def content(content_id):
     content_dict = {}
-    notice = wp.send_query("SELECT * from notice_board")
-    print('sdagsadg')
-    print(notice)
-    max_page = len(notice)
-    page = content_id
-    content_dict['max_page'] = max_page
-    content_dict['page'] = page
+    
+    notice = wp.send_query("SELECT * from notice_board ORDER BY date DESC")
+    page_list = [i['id'] for i in notice]
+    content_dict['page_list'] = page_list
+    content_dict['page'] = content_id
+    
     content = wp.send_query("SELECT * from notice_board where id ={}".format(content_id))
     content_dict['content'] = content[0]
     return render_template('myinfo/myinfo_notice_content.html',contented=content_dict)
